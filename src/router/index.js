045c6b50943c70dea7192f79666b30a6b5160ad0
Joshinator133 <joshua.hoehn@posteo.de>
2023-04-19 18:14:56 +0200

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,42 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import IndexView from '../views/IndexView.vue'
+import ImprintView from '../views/ImprintView.vue'
+import BeantwortenView from '../views/BeantwortenView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: IndexView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/imprint',
+    name: 'imprint',
+    component: ImprintView
+  },
+  {
+    path: '/beantworten',
+    name: 'beantworten',
+    component: BeantwortenView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
 export default router
+
