3fcb0a86f8fcd8c7dcb4ed32e91876fecc9b783d
Joshinator133 <joshua.hoehn@posteo.de>
2023-04-23 09:42:37 +0200

    Umfragen erstellen 1.0

--- src/views/FragenErstellen.vue
+++ src/views/FragenErstellen.vue
@@ -3,16 +3,16 @@
     <h1>Fragen erstellen</h1>
     <form>
       <div>
-        <label>Fragestellung:</label>
-        <input type="text" v-model="frageFragestellung">
+        <label for="fragestellung">Fragestellung:</label>
+        <input id="fragestellung" type="text" v-model="frageFragestellung" required>
       </div>
       <br>
       <div>
-        <label>Antwortoptionen:</label>
-        <input type="text" v-model="frageAntwortoptionen">
+        <label for="antworten">Antwortoptionen:</label>
+        <input id="antworten" type="text" v-model="frageAntwortoptionen" required>
       </div>
       <br>
-      <button @click="addFrage()">Frage hinzufügen</button>
+      <button @click="addFrage()" :disabled="addingFrage">Frage hinzufügen</button>
     </form>
 
     <button  @click="this.$router.push('/'), sessionReset()">Fertig (SessionStorage löschen!)</button>
@@ -24,19 +24,46 @@
   import { doc, addDoc, collection, setDoc} from 'firebase/firestore';
   import { db } from '@/firebase'
   const umfragenCollectionRef = collection(db, 'AlleUmfragen', sessionStorage.getItem('EMailAdmin'), 'Umfragen')
+  const frageFragestellung = ref('')
+  const frageAntwortoptionen = ref('')
+  const addingFrage = ref(false)
 
   const addFrage = async () => {
+    addingFrage.value = true
+    try {
+      console.log("")
+      const umfrageDocRef = doc(umfragenCollectionRef, sessionStorage.getItem('umfragenName'))
+      const fragenCollectionRef = collection(umfrageDocRef, 'Fragen')
+      console.log("FragenCollectionRef: " + fragenCollectionRef)
+      await addDoc(fragenCollectionRef, {
+        question: frageFragestellung.value,
+        options: frageAntwortoptionen.value,
+        selected: []
+      })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      addingFrage.value = false
+    }
+    frageFragestellung.value = ''
+    frageAntwortoptionen.value = ''
+  }
+/*
+  const addFrage = async () => {
+    const selected = []
     console.log("")
     const umfrageDocRef = doc(umfragenCollectionRef, sessionStorage.getItem('umfragenName'))
         const fragenCollectionRef = collection(umfrageDocRef, 'Fragen')
         console.log("FragenCollectionRef: " + fragenCollectionRef)
         await addDoc(fragenCollectionRef, {
-            question: frageFragestellung.value,
-            options: frageAntwortoptionen.value
+           question: frageFragestellung.value,
+           options: frageAntwortoptionen.value,
+           selected: selected
         })
     }
-
+*/
     const sessionReset = () => {
         sessionStorage.removeItem('umfragenName')
     }
+    
 </script>
