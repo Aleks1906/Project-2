8fbcfaf549c127f7ab9af3203acbbbc6d4cb2450
Aleks1906 <99407925+Aleks1906@users.noreply.github.com>
2023-04-23 11:29:11 +0200

    UmfragenÜbersichtUser done

--- src/views/UmfragenÜbersichtUser.vue
+++ src/views/UmfragenÜbersichtUser.vue
@@ -1,28 +1,46 @@
 <template>
-    Test
+    <h1>Übersicht aller Umfragen</h1>
+
+    <div v-for="umfragenarray in umfragen">
+        <div v-for="umfrage in umfragenarray">
+            {{ umfrage }}
+        </div>
+    </div>
 
 </template>
 
-<script>
-/*
+<script setup>
     import { ref, computed, onMounted, resolveDirective } from 'vue'
     import {collection, onSnapshot, doc, updateDoc, FieldValue, arrayUnion, getFirestore} from 'firebase/firestore';
-  
     import { db } from '@/firebase'
  
     const umfragenCollectionRef = collection(db,'AlleUmfragen')
-
-    const umfrageNamen = ref([])
+    const admins = ref([])
+    const umfragen = ref([])
 
     onMounted(() => {
-    umfragenCollectionRef.get().then((querySnapshot) => {
-        const namen = []
-        querySnapshot.forEach((doc) => {
-        const umfrageName = doc.data().name
-        namen.push(umfrageName)
+        onSnapshot(umfragenCollectionRef, (querySnapshot) => {
+            const allAdmins = []
+            querySnapshot.forEach((doc) => {
+                
+                console.log("All Aminds " + doc.id)
+                allAdmins.push(doc.id)
+                console.log( "allAdmins" + allAdmins)
+            })
+            admins.value = allAdmins
+            console.log("Admins.value " + admins.value)
+            admins.value.forEach((admin) => {
+                const allSurveys = []
+                const adminCollectionRef = collection(db, 'AlleUmfragen', admin, 'Umfragen')
+                console.log("Admincollectionref " + adminCollectionRef)
+                onSnapshot(adminCollectionRef, (querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        allSurveys.push(doc.id)
+                        
+                    })
+                    umfragen.value.push(allSurveys)
+                })
+            })
         })
-        umfrageNamen.value = namen
     })
-    })
-   */ 
 </script>
